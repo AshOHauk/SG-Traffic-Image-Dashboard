fd3bc31d7aba3e4861ff22fbf1046bff91cc2bd7
Ash O Hauk <ashohauk@AshOHaukL23>
2024-01-08 11:13:04 +0800

    updated routes to use new base url

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Route, Router } from '@solidjs/router';
-// import banner from './assets/skyscape_generated_example.png'
 import { lazy } from 'solid-js';
 import { PageHeader } from './layouts/PageHeader';
 import { SearchProvider } from './contexts/SearchContext';
@@ -16,8 +15,8 @@ function App() {
       <div class="container overflow-y-hidden w-screen mx-auto">
         <div class="mt-16">
         <Router>
-          <Route path="/" component={Home} />
-          <Route path="/about" component={About} />
+          <Route path="/SG-Traffic-Image-Dashboard/" component={Home} />
+          <Route path="/SG-Traffic-Image-Dashboard/about" component={About} />
         </Router>
         <div class="text-center text-sm">Ashsiddique Omrul Hauk</div>
         </div>
